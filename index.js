92d9a8f2902504b5971f90c5cb9c2bff68b653a0
Murthi86 <kutti45@gmail.com>
2020-07-18 10:14:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let chunkstring = "0+612,612+768,1380+768";
+let chunks = chunkstring.split(",")
+console.log(chunks);
+
+  let [offset, length] = chunks[0].split('+');
+  console.log(offset);
+  console.log(length);
